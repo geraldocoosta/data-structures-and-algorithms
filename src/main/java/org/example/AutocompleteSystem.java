bf77f7a2ebbb2bfedce110379c20c7d2f52aafb5
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-08-11 18:41:21 -0300

    Algorithms

---
+++ src/main/java/org/example/AutocompleteSystem.java
@@ -0,0 +1,109 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class AutocompleteSystem {
+    TrieNode root;
+    String prefix;
+
+    public AutocompleteSystem(String[] sentences, int[] times) {
+        root = new TrieNode();
+        prefix = "";
+        for (int i = 0; i < sentences.length; i++) {
+            add(sentences[i], times[i]);
+        }
+    }
+
+    public List<String> input(char c) {
+        if (c == '#') {
+            add(prefix, 1);
+            prefix = "";
+            return new ArrayList<>();
+        }
+
+        prefix = prefix + c;
+        TrieNode curr = root;
+        for (char cc : prefix.toCharArray()) {
+            TrieNode next = curr.children.get(cc);
+            if (next == null) {
+                return new ArrayList<>();
+            }
+            curr = next;
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> (a.c == b.c ? a.s.compareTo(b.s) : b.c - a.c));
+        for (String s : curr.counts.keySet()) {
+            pq.add(new Pair(s, curr.counts.get(s)));
+        }
+
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < 3 && !pq.isEmpty(); i++) {
+            res.add(pq.poll().s);
+        }
+        return res;
+    }
+
+    private void add(String s, int count) {
+        TrieNode curr = root;
+        for (char c : s.toCharArray()) {
+            TrieNode next = curr.children.get(c);
+            if (next == null) {
+                next = new TrieNode();
+                curr.children.put(c, next);
+            }
+            curr = next;
+            curr.counts.put(s, curr.counts.getOrDefault(s, 0) + count);
+        }
+        curr.isWord = true;
+    }
+
+    class TrieNode {
+        Map<Character, TrieNode> children;
+        Map<String, Integer> counts;
+        boolean isWord;
+
+        public TrieNode() {
+            children = new HashMap<>();
+            counts = new HashMap<>();
+            isWord = false;
+        }
+    }
+
+    class Pair {
+        String s;
+        int c;
+
+        public Pair(String s, int c) {
+            this.s = s;
+            this.c = c;
+        }
+    }
+}
+
+/*
+*
+* 642. Design Search Autocomplete System
+*
+* Design a search autocomplete system for a search engine. Users may input a sentence (at least one word and end with a special character '#').
+
+You are given a string array sentences and an integer array times both of length n where sentences[i] is a previously typed sentence and times[i] is the corresponding number of times the sentence was typed. For each input character except '#', return the top 3 historical hot sentences that have the same prefix as the part of the sentence already typed.
+
+Here are the specific rules:
+
+The hot degree for a sentence is defined as the number of times a user typed the exactly same sentence before.
+The returned top 3 hot sentences should be sorted by hot degree (The first is the hottest one). If several sentences have the same hot degree, use ASCII-code order (smaller one appears first).
+If less than 3 hot sentences exist, return as many as you can.
+When the input is a special character, it means the sentence ends, and in this case, you need to return an empty list.
+Implement the AutocompleteSystem class:
+
+AutocompleteSystem(String[] sentences, int[] times) Initializes the object with the sentences and times arrays.
+List<String> input(char c) This indicates that the user typed the character c.
+Returns an empty array [] if c == '#' and stores the inputted sentence in the system.
+Returns the top 3 historical hot sentences that have the same prefix as the part of the sentence already typed. If there are fewer than 3 matches, return them all.
+*
+*
+* */
