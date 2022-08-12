35b1af222102695d069fdec46329950b027361b5
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-08-12 18:29:35 -0300

    Algorithms

---
+++ src/main/java/org/example/ConcatenatedWords.java
@@ -0,0 +1,80 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ConcatenatedWords {
+
+    public static void main(String[] args) {
+        System.out.println(new ConcatenatedWords().findAllConcatenatedWordsInADict(new String[]{"cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"}));
+    }
+
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        List<String> result = new ArrayList<>();
+        Set<String> preWords = new HashSet<>();
+        Arrays.sort(words, Comparator.comparingInt(String::length));
+
+        for (String word : words) {
+            if (canForm(word, preWords)) {
+                result.add(word);
+            }
+            preWords.add(word);
+        }
+
+        return result;
+    }
+
+    private static boolean canForm(String word, Set<String> dict) {
+        if (dict.isEmpty()) return false;
+        boolean[] dp = new boolean[word.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= word.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (!dp[j]) {
+                    continue;
+                }
+                if (dict.contains(word.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[word.length()];
+    }
+}
+
+/*
+*
+* Given an array of strings words (without duplicates), return all the concatenated words in the given list of words.
+
+A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
+
+
+
+Example 1:
+
+Input: words = ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats";
+"dogcatsdog" can be concatenated by "dog", "cats" and "dog";
+"ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+Example 2:
+
+Input: words = ["cat","dog","catdog"]
+Output: ["catdog"]
+
+
+Constraints:
+
+1 <= words.length <= 104
+1 <= words[i].length <= 30
+words[i] consists of only lowercase English letters.
+All the strings of words are unique.
+1 <= sum(words[i].length) <= 105
+*
+*
+* */
