53532317499da8802ef797deeb3f56e2bc27742c
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-08-15 11:42:55 -0300

    Algorithms

---
+++ src/main/java/org/example/GenerateParentheses.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        System.out.println(new GenerateParentheses().generateParenthesis(3));
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
+    }
+}
+
+/*
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+
+
+Constraints:
+
+1 <= n <= 8
+
+
+*/
---
+++ src/main/java/org/example/WordSearch.java
@@ -0,0 +1,82 @@
+package org.example;
+
+
+import java.util.ArrayList;
+
+
+public class WordSearch {
+
+    char[][] board = null;
+    ArrayList<String> result = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println(new WordSearch()
+                .exist(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}
+                        , "ABCCED"));
+    }
+
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[0].length; j++) {
+                if (exist(board, i, j, word, 0))
+                    return true;
+            }
+        return false;
+    }
+
+    private boolean exist(char[][] board, int i, int j, String word, int ind) {
+        if (ind == word.length()) return true;
+        if (i > board.length - 1 || i < 0 || j < 0 || j > board[0].length - 1 || board[i][j] != word.charAt(ind))
+            return false;
+        board[i][j] = '*';
+        boolean result = exist(board, i - 1, j, word, ind + 1) ||
+                exist(board, i, j - 1, word, ind + 1) ||
+                exist(board, i, j + 1, word, ind + 1) ||
+                exist(board, i + 1, j, word, ind + 1);
+        board[i][j] = word.charAt(ind);
+        return result;
+    }
+
+}
+
+
+/*
+
+Word Search
+
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+
+
+Follow up: Could you use search pruning to make your solution faster with a larger board?
+
+
+*/
