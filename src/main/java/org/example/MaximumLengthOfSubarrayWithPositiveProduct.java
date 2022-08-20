4181f03226f10fedcdf72e3541ed13ea90db94c0
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-08-20 11:04:25 -0300

    Algorithms

--- src/main/java/org/example/MaximumLengthOfSubarrayWithPositiveProduct.java
+++ src/main/java/org/example/MaximumLengthOfSubarrayWithPositiveProduct.java
@@ -3,29 +3,44 @@ package org.example;
 public class MaximumLengthOfSubarrayWithPositiveProduct {
 
     public static void main(String[] args) {
-        System.out.println(new MaximumLengthOfSubarrayWithPositiveProduct().getMaxLen(new int[]{1,-2,-3,4}));
+        System.out.println(new MaximumLengthOfSubarrayWithPositiveProduct().getMaxLen(new int[]{1, -2, -3, 4}));
     }
 
-    public static int getMaxLen(int[] nums) {
-        int maxLen = 0;
-        int pos = 0;
-        int neg = 0;
-        for (int i : nums) {
-            if (i < 0) {
-                int temp = pos;
-                pos = neg == 0 ? 0 : neg + 1;
-                neg = temp + 1;
-            } else if (i == 0) {
-                pos = 0;
-                neg = 0;
-            } else {
-                pos++;
-                neg = neg == 0 ? 0 : neg + 1;
-            }
-            maxLen = Math.max(maxLen, pos);
-        }
-        return maxLen;
+    // Time O(n)
+    // Space O(1)
 
+    public int getMaxLen(int[] nums) {
+        // sum is used to count the number of negative numbers from zeroPosition to current index
+        int firstNegative = -1;
+        int zeroPosition = -1;
+        int cntNegative = 0;
+        int max = 0;
+
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                cntNegative++;
+                // we only need to know index of first negative number
+                if (firstNegative == -1) {
+                    firstNegative = i;
+                }
+            }
+            // if current number is 0, we can't use any element from index 0 to i anymore, so update zeroPosition,
+            // and reset sum and firstNegative. If it is a game, we should refresh the game when we meet 0.
+            if (nums[i] == 0) {
+                cntNegative = 0;
+                firstNegative = -1;
+                zeroPosition = i;
+            } else {
+                // consider index of zero
+                if (cntNegative % 2 == 0) {
+                    max = Math.max(i - zeroPosition, max);
+                } else { // consider index of first negative number
+                    max = Math.max(i - firstNegative, max);
+                }
+            }
+        }
+        return max;
     }
 }
 
