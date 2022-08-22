610343300193772a7d9f8b52b25e0a4bd0123263
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-08-22 16:59:15 -0300

    Algorithms

---
+++ src/main/java/org/example/UniquePaths.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class UniquePaths {
+
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 2;
+        System.out.println(uniquePaths(m, n));
+        System.out.println(uniquePaths2(m, n));
+    }
+
+    public static int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+
+    public static int uniquePaths2(int m, int n) {
+        int[][] d = new int[m][n];
+
+        for (int[] arr : d) {
+            Arrays.fill(arr, 1);
+        }
+        for (int col = 1; col < m; ++col) {
+            for (int row = 1; row < n; ++row) {
+                d[col][row] = d[col - 1][row] + d[col][row - 1];
+            }
+        }
+        return d[m - 1][n - 1];
+    }
+}
+
+/*
+*
+* There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+*
+* */
