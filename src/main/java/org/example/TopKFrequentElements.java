49a6aa433dcba7f966f8fbb754456055f88065bc
Geraldo Costa <geraldocosta@ciandt.com>
2022-08-22 12:28:40 -0300

    Algorithms

--- src/main/java/org/example/TopKFrequentElements.java
+++ src/main/java/org/example/TopKFrequentElements.java
@@ -9,21 +9,20 @@ import java.util.Queue;
 public class TopKFrequentElements {
 
     public static void main(String[] args) {
-        int[] nums = {1,1,1,2,2,3};
+        int[] nums = {1, 1, 1, 2, 2, 3};
         int k = 2;
         System.out.println(Arrays.toString(topKFrequent(nums, k)));
     }
 
     public static int[] topKFrequent(int[] nums, int k) {
-        // O(1) time
         if (k == nums.length) {
             return nums;
         }
 
         // 1. build hash map : character and how often it appears
         // O(N) time
-        Map<Integer, Integer> count = new HashMap();
-        for (int n: nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int n : nums) {
             count.put(n, count.getOrDefault(n, 0) + 1);
         }
 
@@ -33,7 +32,7 @@ public class TopKFrequentElements {
 
         // 2. keep k top frequent elements in the heap
         // O(N log k) < O(N log N) time
-        for (int n: count.keySet()) {
+        for (int n : count.keySet()) {
             heap.add(n);
             if (heap.size() > k) heap.poll();
         }
@@ -41,7 +40,7 @@ public class TopKFrequentElements {
         // 3. build an output array
         // O(k log k) time
         int[] top = new int[k];
-        for(int i = k - 1; i >= 0; --i) {
+        for (int i = k - 1; i >= 0; --i) {
             top[i] = heap.poll();
         }
         return top;
