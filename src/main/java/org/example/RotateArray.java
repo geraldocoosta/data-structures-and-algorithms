28e2cdc0f0758260e4f9d6bd159b1219dc3db70c
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-09-04 20:14:29 -0300

    Algorithms

--- src/main/java/org/example/RotateArray.java
+++ src/main/java/org/example/RotateArray.java
@@ -26,7 +26,7 @@ public class RotateArray {
         var result = new int[nums.length];
 
         for (int i = 0; i < nums.length; i++) {
-            var destiny = (i+k) % nums.length;
+            var destiny = (i + k) % nums.length;
             result[destiny] = nums[i];
         }
 
@@ -48,7 +48,7 @@ public class RotateArray {
         // inverto o array completamente
         while (left < right) {
             var aux = nums[right];
-            nums[right] =  nums[left];
+            nums[right] = nums[left];
             nums[left] = aux;
             left++;
             right--;
@@ -60,7 +60,7 @@ public class RotateArray {
 
         while (left < right) {
             var aux = nums[right];
-            nums[right] =  nums[left];
+            nums[right] = nums[left];
             nums[left] = aux;
             left++;
             right--;
@@ -71,7 +71,7 @@ public class RotateArray {
         right = nums.length - 1;
         while (left < right) {
             var aux = nums[right];
-            nums[right] =  nums[left];
+            nums[right] = nums[left];
             nums[left] = aux;
             left++;
             right--;
@@ -80,3 +80,41 @@ public class RotateArray {
 
 
 }
+
+/* Rotate Array
+*
+* Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+
+
+Follow up:
+
+Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+*
+*
+* */
