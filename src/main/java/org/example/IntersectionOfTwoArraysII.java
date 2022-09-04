7c4b8d202e1f842de3d6d2c4b0719408a17cf39f
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-09-04 20:26:37 -0300

    Algorithms

---
+++ src/main/java/org/example/IntersectionOfTwoArraysII.java
@@ -0,0 +1,95 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class IntersectionOfTwoArraysII {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+        System.out.println(Arrays.toString(intersect2(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+    }
+
+    // O(n+m)
+    // O(min(n,m))
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        HashMap<Integer, Integer> m = new HashMap<>();
+        for (int n : nums1) {
+            m.put(n, m.getOrDefault(n, 0) + 1);
+        }
+        int k = 0;
+        for (int n : nums2) {
+            int cnt = m.getOrDefault(n, 0);
+            if (cnt > 0) {
+                nums1[k] = n;
+                k++;
+                m.put(n, cnt - 1);
+            }
+        }
+
+        var result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = nums1[i];
+        }
+        return result;
+    }
+
+    // O(n log n + m log m)
+    // O(log n + log m)
+    public static int[] intersect2(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                ++i;
+            } else if (nums1[i] > nums2[j]) {
+                ++j;
+            } else {
+                nums1[k] = nums1[i];
+                k++;
+                i++;
+                ++j;
+            }
+        }
+        var result = new int[k];
+        for (int index = 0; index < k; index++) {
+            result[index] = nums1[index];
+        }
+        return result;
+    }
+}
+
+/*
+* Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+
+
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+* */
