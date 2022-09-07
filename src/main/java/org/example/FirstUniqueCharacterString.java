4e1a1076e147ce7d94e478c7a14a360879770d6f
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-09-07 09:57:00 -0300

    Algorithms

---
+++ src/main/java/org/example/FirstUniqueCharacterString.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class FirstUniqueCharacterString {
+
+	public static void main(String[] args) {
+
+		System.out.println(new FirstUniqueCharacterString().firstUniqChar("ihaaaai"));
+	}
+
+	public int firstUniqChar(String s) {
+
+		int[] count = new int[26];
+		int n = s.length();
+		for (int i = 0; i < n; i++) {
+			int c = s.charAt(i) - 'a';
+			count[c]++;
+		}
+
+		// find the index
+		for (int i = 0; i < s.length(); i++) {
+			if (count[s.charAt(i) - 'a'] == 1) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}
+
+/*
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+* */
---
+++ src/main/java/org/example/ValidAnagram.java
@@ -0,0 +1,85 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+	public static void main(String[] args) {
+		String s = "anagram";
+		String t = "nagaram";
+		System.out.println(isAnagram(s, t));
+		System.out.println(isAnagram1(s, t));
+	}
+
+
+	// O(n log n)
+	// O(1)
+	public static boolean isAnagram(String s, String t) {
+		if (s.length() != t.length()) {
+			return false;
+		}
+		char[] str1 = s.toCharArray();
+		char[] str2 = t.toCharArray();
+		Arrays.sort(str1);
+		Arrays.sort(str2);
+		return Arrays.equals(str1, str2);
+	}
+
+	// O(n)
+	// O(1)
+	public static boolean isAnagram1(String s, String t) {
+		if (s.length() != t.length()) {
+			return false;
+		}
+		int[] table = new int[26];
+		for (int i = 0; i < s.length(); i++) {
+			table[s.charAt(i) - 'a']++;
+		}
+		for (int i = 0; i < t.length(); i++) {
+			table[t.charAt(i) - 'a']--;
+			if (table[t.charAt(i) - 'a'] < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+/*
+*
+* 242. Valid Anagram
+Easy
+
+6855
+
+238
+
+Add to List
+
+Share
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+*
+*
+* */
