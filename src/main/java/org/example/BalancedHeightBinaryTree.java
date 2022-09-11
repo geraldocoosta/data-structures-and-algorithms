648eec292304393442b3a16e2e56678c054e6e1f
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-09-11 19:42:33 -0300

    Algorithms

---
+++ src/main/java/org/example/BalancedHeightBinaryTree.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class BalancedHeightBinaryTree {
+
+	public static void main(String[] args) {
+
+		TreeNode treeNode = new TreeNode(1);
+		treeNode.left = new TreeNode(2);
+		treeNode.right = new TreeNode(2);
+		treeNode.left.left = new TreeNode(3);
+		treeNode.left.right = new TreeNode(3);
+		treeNode.left.left.left = new TreeNode(4);
+		treeNode.left.left.right = new TreeNode(4);
+
+		System.out.println(isBalanced(treeNode));
+	}
+
+	public static boolean isBalanced(TreeNode root) {
+		return dfsHeight(root) != -1;
+	}
+
+	public static int dfsHeight(TreeNode root) {
+
+		if (root == null) {
+			return 0;
+		}
+
+		var resultLeft = dfsHeight(root.left);
+		if (resultLeft == -1) {
+			return -1;
+		}
+
+		var resultRight = dfsHeight(root.right);
+		if (resultRight == -1) {
+			return -1;
+		}
+
+		if (Math.abs(resultLeft - resultRight) > 1) {
+			return -1;
+		}
+
+		return Math.max(resultLeft, resultRight) + 1;
+
+	}
+}
