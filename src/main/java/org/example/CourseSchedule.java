eda064513363269431bbc28e96001c78f91d1d5b
Geraldo Costa <jose.costa@basis.com.br>
2022-10-02 17:13:14 -0300

    Algo

--- src/main/java/org/example/CourseSchedule.java
+++ src/main/java/org/example/CourseSchedule.java
@@ -1,84 +1,64 @@
 package org.example;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class CourseSchedule {
 
     public static void main(String[] args) {
-        CourseSchedule c = new CourseSchedule();
-        System.out.println(c.canFinish(2, new int[][]{{1, 0}, {0, 1}}));
+        System.out.println(
+                canFinish(5, new int[][]{
+                        {0,1},
+                        {0,2},
+                        {2,3},
+                        {2,4},
+                        {3,4}
+                })
+        );
+
     }
 
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
 
-    // Time O(V*E)
-    // Space O(V)
+        var graph = new HashMap<Integer, List<Integer>>();
 
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[][] matrix = new int[numCourses][numCourses]; // i -> j
-        int[] indegree = new int[numCourses];
+        var visited = new boolean[numCourses];
+        var visitedDfs = new boolean[numCourses];
 
-        for (int i = 0; i < prerequisites.length; i++) {
-            int ready = prerequisites[i][0];
-            int pre = prerequisites[i][1];
-            if (matrix[pre][ready] == 0) {
-                indegree[ready]++;
-            }
-            matrix[pre][ready] = 1;
+        for (int i = 0; i < numCourses; i++) {
+            graph.put(i, new ArrayList<>());
         }
 
-        int count = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < indegree.length; i++) {
-            if (indegree[i] == 0) queue.add(i);
+        for (int[] prerequisite : prerequisites) {
+            graph.get(prerequisite[0]).add(prerequisite[1]);
         }
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            count++;
-            for (int i = 0; i < numCourses; i++) {
-                if (matrix[course][i] != 0) {
-                    if (--indegree[i] == 0) {
-                        queue.add(i);
-                    }
-                }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i]) continue;
+            if (hasCycle(graph, visited, visitedDfs, i)) {
+                return false;
             }
         }
-        return count == numCourses;
+        return true;
+
     }
+
+    private static boolean hasCycle(Map<Integer, List<Integer>> graph, boolean[] visited, boolean[] visitedDfs, int course) {
+        if (visitedDfs[course]) {
+            return true;
+        }
+
+        visitedDfs[course] = true;
+
+        for (int nextCourse : graph.get(course)) {
+            if (hasCycle(graph, visited, visitedDfs, nextCourse)) {
+                return true;
+            }
+        }
+
+        visitedDfs[course] = false;
+        visited[course] = true;
+
+        return false;
+    }
+
 }
-
-/*
-
-Course Schedule
-
-There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
-
-For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
-Return true if you can finish all courses. Otherwise, return false.
-
-
-
-Example 1:
-
-Input: numCourses = 2, prerequisites = [[1,0]]
-Output: true
-Explanation: There are a total of 2 courses to take.
-To take course 1 you should have finished course 0. So it is possible.
-Example 2:
-
-Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
-Output: false
-Explanation: There are a total of 2 courses to take.
-To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
-
-
-Constraints:
-
-1 <= numCourses <= 2000
-0 <= prerequisites.length <= 5000
-prerequisites[i].length == 2
-0 <= ai, bi < numCourses
-All the pairs prerequisites[i] are unique.
-
-
-*/
